5e3d90eefaf9f975bc9766f1a4a8111a3c673190
Fernando Portillo <fernenri33@gmail.com>
2025-11-08 20:59:29 -0600

    fix de cat policy

--- app/Policies/CategoryPolicy.php
+++ app/Policies/CategoryPolicy.php
@@ -19,7 +19,7 @@ class CategoryPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(?User $user, Category $category): bool
+    public function view(?User $user): bool
     {
         return true;
     }
